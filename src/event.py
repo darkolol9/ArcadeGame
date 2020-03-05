5f17143c3fa530c063c6bd31390f1d3a6b34b124
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2020-03-05 08:38:29 +0200

    ghbfghfgh
    fghfgh

--- src/event.py
+++ src/event.py
@@ -4,3 +4,38 @@ def event_handler():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+def menu_handler(playrect,exitrect,screen):
+    mouse = pygame.mouse.get_pos()
+
+    if pygame.mouse.get_pressed()[0]:
+        if playrect.collidepoint(mouse[0],mouse[1]):
+            screen.fill((0,0,100))
+            return True
+        elif exitrect.collidepoint(mouse[0],mouse[1]):
+            print('exiting')
+            exit()
+    return False
+
+
+def menu(screen):
+    font = pygame.font.Font('freesansbold.ttf', 60)
+
+    play = font.render('PLAY', True, (255,255,255), (255,0,0)) 
+
+    playRect = play.get_rect() 
+    playRect.center = (250,100)
+
+    Exit = font.render('EXIT', True, (255,255,255), (255,0,0)) 
+
+    ExitRect = Exit.get_rect() 
+    ExitRect.center = (250,200)
+
+    screen.blit(play,playRect)
+    screen.blit(Exit,ExitRect)
+
+    flag = menu_handler(playRect,ExitRect,screen)
+    return flag
+
+    
+
--- src/main.py
+++ src/main.py
@@ -1,22 +1,36 @@
 import pygame
 from event import event_handler
+from event import menu_handler
+from event import menu
 import sprites
 
 
-
 pygame.init()
+pygame.display.set_caption(' GAME WINDOW ')
 
 width,height = 700,700
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width,height))
-block = sprites.block(60,60)
+block = sprites.block(40,40)
+menu_screen_is_over = False
+
 #game loop
 while True:
-    screen.fill((0,0,0))
 
+    screen.fill((0,0,100))
     #eventhandler goes here
     event_handler()
-    block.update()
-    block.blit(screen)
-    #updates
+
+    if menu_screen_is_over:
+
+        #updates
+
+        block.update()
+        block.blit(screen)
+        clock.tick(60)
+        
+    else :
+        menu_screen_is_over =  menu(screen)
+        
 
     pygame.display.flip()
